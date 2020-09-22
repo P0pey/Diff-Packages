76f0854e0182066d2e929e52f7601927185b469a
Maxime Chardon <maxime.chardon@epita.fr>
2020-09-22 12:41:38 +0200

    modifying little options

--- diff_cleaner.py
+++ diff_cleaner.py
@@ -3,12 +3,12 @@
 diff_file = "diff.txt"
 
 f = open(diff_file)
-fo = open("missing_packages", "w")
+fo = open("missing_pkg", "w")
 L = f.readlines()
 f.close()
 
 for line in L:
     if line[0] == '<':
         fo.write(line[2::])
-        print("Missing package ===> " + line[2::])
+        print("Missing package ===> " + line[2::-1])
 fo.close()
--- get_diff.sh
+++ get_diff.sh
@@ -4,13 +4,22 @@
 wget https://ipxe.pie.cri.epita.fr/cri-pxe-images.s3.cri.epita.fr/pie-archlinux-pkglist.txt
 
 # Get your packages list
-sudo pacman -Q > my_pkg.txt
+sudo pacman -Q > my_pkg
 
 # Run diff files
-diff pie-archlinux-pkglist.txt my_pkg.txt > diff.txt
+diff pie-archlinux-pkglist.txt my_pkg > diff
 
 # Get diff_cleaner python script
 wget https://raw.githubusercontent.com/mxmchdn/Diff-Packages/master/diff_cleaner.py
+# remove if already exists
+rm diff_cleaner.py.*
 
 # Analyse it and get missing packages
 python3 diff_cleaner.py
+
+# move files
+mkdir Output
+mv diff Output/
+mv my_pkg Output/
+mv pie-archlinux-pkglist.txt Output/
+mv missing_pkg Output/
